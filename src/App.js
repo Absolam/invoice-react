2d31a9a623faa87d169569a2d5004fda6603d944
Aaron A <aaronabsolam@yahoo.com>
2022-05-06 13:05:25 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { View } from "./Pages/View";
+import { Edit } from "./Pages/Edit";
+import { Create } from "./Pages/Create";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/view" element={<View />} />
+        <Route path="/edit" element={<Edit />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
+    </Router>
   );
 }
 
